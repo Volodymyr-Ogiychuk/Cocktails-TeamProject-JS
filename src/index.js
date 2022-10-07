9f4a6930e33286a64945a685f212aca337d1a799
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-07 14:37:15 +0300

    js files added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as searchByName from './js/Alex/searchByName';
+import * as authorization from './js/Alex/authorization';
+import * as renderLettersFilter from './js/Ira/renderLettersFilter';
+import * as favoriteCocktails from './js/Vitalik/favoriteCocktails';
+import * as favoriteIngridients from './js/Vitalik/favoriteIngridients';
+import * as themeChanger from './js/Vitalik/themeChanger';
+import * as pagination from './js/Volodymyr/pagination';
+import * as searchByLetter from './js/Volodymyr/searchByLetter';
