c77031067328058d6e68fbb9bc2dda850d906e77
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-08 12:26:40 +0300

    fetch

---
+++ src/js/getCocktails.js
@@ -0,0 +1,46 @@
+export function getCocktailByName(cocktailName) {
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  });
+}
+
+export function getCocktailByLetter(cocktailLetter) {
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${cocktailLetter}`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  });
+}
+
+export function getIngrByName(ingrName) {
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingrName}`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  });
+}
+
+export function getRandomCocktail() {
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`).then(
+    response => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    }
+  );
+}
+
+export function getIngrImg(inrgName) {
+  // there 3 img sizes available and function must receive parameter as follow: "gin-Small.png" or "gin-Medium.png" or "gin.png"
+
+  return fetch(
+    `https://www.thecocktaildb.com/images/ingredients/${inrgName}`
+  ).then(response => {
+    if (!response.ok) throw new Error(response.status);
+    return response.url;
+  });
+}
