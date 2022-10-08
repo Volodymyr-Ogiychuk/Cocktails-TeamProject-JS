416c02d64c097833d1c022c59d5bd65ad23967e5
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-08 15:59:23 +0300

    added search by name
    need to show search fail message.

--- src/js/Alex/searchByName.js
+++ src/js/Alex/searchByName.js
@@ -1,4 +1,24 @@
 import { getCocktailByName } from '../getCocktails';
-console.log('helloworld');
+import { renderDrinkMarkup } from '../markupTools';
 
-console.log(getIngrImg('gin-Small.png').then(console.log));
+const searchFormRef = document.querySelector('#search-form');
+const cocktailsListRef = document.querySelector('.cocktails__list');
+const searchFailRef = document.querySelector('selector');
+
+searchFormRef.addEventListener('submit', onSearchSubmit);
+
+function onSearchSubmit(e) {
+  e.preventDefault();
+  const userInput = searchFormRef.elements.searchQuery.value;
+  // searchFailRef.classList.add('is-hidden');
+
+  getCocktailByName(userInput)
+    .then(({ drinks }) => {
+      cocktailsListRef.innerHTML = renderDrinkMarkup(drinks);
+    })
+    .catch(error => {
+      console.log(error);
+      // display 'Sorry, we didn't find any cocktail for you'
+      // searchFailRef.classList.remove('is-hidden');
+    });
+}
