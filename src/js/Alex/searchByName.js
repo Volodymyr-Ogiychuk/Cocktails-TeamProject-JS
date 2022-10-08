22dedc4358a8c6cbbddcec8fafc3f99a06983e2e
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-08 14:09:11 +0300

    Update searchByName.js

--- src/js/Alex/searchByName.js
+++ src/js/Alex/searchByName.js
@@ -1,4 +1,4 @@
-import { getIngrImg } from '../getCocktails';
+import { getCocktailByName } from '../getCocktails';
 console.log('helloworld');
 
 console.log(getIngrImg('gin-Small.png').then(console.log));
