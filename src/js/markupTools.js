97fc65d78018e4d0be0a3f3d896cc16663c07a30
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-12 12:17:41 +0300

    Update markupTools.js

--- src/js/markupTools.js
+++ src/js/markupTools.js
@@ -1,11 +1,11 @@
 export function renderDrinkMarkup(drinksArr) {
   return drinksArr
-    .map(({ strDrink, strDrinkThumb }) => {
+    .map(({ strDrink, strDrinkThumb, idDrink }) => {
       return `<li class="cocktails__card">
               <div>
                 <img src="${strDrinkThumb}" alt="${strDrink}" width="280">
               </div>
-              <p class="cocktails__name">${strDrink}</p>
+              <p class="cocktails__name" data-id="${idDrink}">${strDrink}</p>
               <div class="button-wrapper">
                 <button class="btn-lm" type="button">Learn more</button>
                 <button class="btn-add" type="button">Add to
