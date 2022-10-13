fc437d5786ee70ee0c9b2a6badbff70960500764
vitaliksmile <vitaliksmile777@gmail.com>
2022-10-13 17:25:09 +0300

    update

--- src/js/Vitalik/favoriteCocktails.js
+++ src/js/Vitalik/favoriteCocktails.js
@@ -5,25 +5,10 @@ const btnAdd = document.querySelector('.cocktails.section');
 btnAdd.addEventListener('click', getFavoritesCoctails);
 
 function getFavoritesCoctails(event) {
-  if (event.target.textContent === 'remove') {
+  if (event.target.textContent === 'Remove') {
     const idCoctails =
       event.target.parentNode.previousElementSibling.dataset.id;
-    //   event.target.textContent = 'Add to';
-    // const svg = document.createElement('svg');
-    // svg.classList.add('icon__add');
-    // const use = document.createElement('use');
-    // use.href = './image/symbol-defs.svg#icon-Heart';
-    // use.role = 'button';
-    // svg.appendChild(use);
-    // console.log(svg);
-    event.target.innerHTML = ` Add to
-                  <svg class="icon__add" width="18" height="18">
-                    <use
-                      href="./image/symbol-defs.svg#icon-Heart"
-                      aria-label="img"
-                      role="button"
-                    ></use>
-                  </svg>`;
+    event.target.textContent = 'Add to';
 
     removeId(idCoctails);
     save(COCKTAILS_STORAGE_KEY, nameCocktails);
@@ -35,7 +20,7 @@ function getFavoritesCoctails(event) {
       event.target.parentNode.previousElementSibling.dataset.id;
     nameCocktails.push(idCoctails);
     save(COCKTAILS_STORAGE_KEY, nameCocktails);
-    event.target.textContent = 'remove';
+    event.target.textContent = 'Remove';
     // console.log(event.target);
     console.log(nameCocktails);
   }
