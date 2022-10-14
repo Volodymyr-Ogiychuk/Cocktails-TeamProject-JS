c3302dadee4eacf7ed8784bf5d028ac2ff4176f0
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-10-14 09:58:10 +0300

    Update searchByLetter.js

--- src/js/Volodymyr/searchByLetter.js
+++ src/js/Volodymyr/searchByLetter.js
@@ -1,6 +1,5 @@
-
 import { getCocktailByLetter } from '../getCocktails';
-import { getCocktailById } from '../getCocktails'; 
+import { getCocktailById } from '../getCocktails';
 import { getRandomCocktail } from '../getCocktails';
 import { getIngrByName } from '../getCocktails';
 import { renderDrinkMarkup } from '../markupTools';
@@ -26,218 +25,217 @@ let ingrMarkup = '';
 let ingrCardMarkup = '';
 let cocktListLength = 0;
 
-
 const str = 'abcdefghijklmnopqrstuvwxyz 1234567890';
-let arr = str.split("");
+let arr = str.split('');
 let arrIngreds = load('ingridients');
-let arrCocktls = load('cocktails');;
-
+let arrCocktls = load('cocktails');
 
 // контроль ширины экрана
 
 function widthControl() {
-    if (window.innerWidth <= 768) {
-        cardsPerPage = 3;
-        formByLetter.classList.add('is-hidden')
-    } else  if (window.innerWidth > 768 && window.innerWidth <= 1280) {
-        cardsPerPage = 6;
-    }
-    cardsPerPageAfterResize = cardsPerPage;
+  if (window.innerWidth <= 768) {
+    cardsPerPage = 3;
+    formByLetter.classList.add('is-hidden');
+  } else if (window.innerWidth > 768 && window.innerWidth <= 1280) {
+    cardsPerPage = 6;
+  }
+  cardsPerPageAfterResize = cardsPerPage;
 
-    
-    window.addEventListener('resize', () => {
-        if (window.innerWidth <= 768) {
-            formByLetter.classList.add('is-hidden')
-            cardsPerPageAfterResize = 3;
-        } else if (window.innerWidth > 768 && window.innerWidth <= 1280) {
-            formByLetter.classList.remove('is-hidden')
-            cardsPerPageAfterResize = 6;
-        } else {
-            cardsPerPageAfterResize = 9;
-        };
-        
-        if (cardsPerPageAfterResize !== cardsPerPage) {
-            cardsPerPage = cardsPerPageAfterResize;
-            cocktList.innerHTML = '';
-            randomDrinks = [];
-            createRandomList();
-        }
-});
-};
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+      formByLetter.classList.add('is-hidden');
+      cardsPerPageAfterResize = 3;
+    } else if (window.innerWidth > 768 && window.innerWidth <= 1280) {
+      formByLetter.classList.remove('is-hidden');
+      cardsPerPageAfterResize = 6;
+    } else {
+      cardsPerPageAfterResize = 9;
+    }
+
+    if (cardsPerPageAfterResize !== cardsPerPage) {
+      cardsPerPage = cardsPerPageAfterResize;
+      cocktList.innerHTML = '';
+      randomDrinks = [];
+      createRandomList();
+    }
+  });
+}
 
 widthControl();
 
 function createLetterList(arr) {
-    const markup = arr.map(element => {
-        return `
+  const markup = arr
+    .map(element => {
+      return `
             <li class="letter">
                 <button class="btn-letter" type="button">
                 ${element}
                 </button>
             </li>`;
-        })
-            .join('');
-    formByLetter.insertAdjacentHTML('beforeend', markup);
-    letterListRef.insertAdjacentHTML('beforeend', markup);
-    btnLetterRef = document.querySelectorAll('.btn-letter');
-       
-};
+    })
+    .join('');
+  formByLetter.insertAdjacentHTML('beforeend', markup);
+  letterListRef.insertAdjacentHTML('beforeend', markup);
+  btnLetterRef = document.querySelectorAll('.btn-letter');
+}
 
 function createRandomList() {
-    
-    cocktList.innerHTML = '';
-    
-        for (let i = 1; i <= cardsPerPage; i += 1) {
-            getRandomCocktail()
-            .then(data => {
-                if (data.drinks !== null) {
-                    cocktListLength = data.drinks.length;
-                    randomDrinks.push(data.drinks[0]);
-                    const cardMarkup = renderDrinkMarkup(randomDrinks);
-                    cocktList.innerHTML = '';
-                    cocktList.insertAdjacentHTML('beforeend', cardMarkup);
-                } 
-            });     
-        }
-    const cardMarkup = renderDrinkMarkup(randomDrinks);
-    cocktList.innerHTML = '';
-    cocktList.insertAdjacentHTML('beforeend', cardMarkup);
-};
+  cocktList.innerHTML = '';
+
+  for (let i = 1; i <= cardsPerPage; i += 1) {
+    getRandomCocktail().then(data => {
+      if (data.drinks !== null) {
+        cocktListLength = data.drinks.length;
+        randomDrinks.push(data.drinks[0]);
+        const cardMarkup = renderDrinkMarkup(randomDrinks);
+        cocktList.innerHTML = '';
+        cocktList.insertAdjacentHTML('beforeend', cardMarkup);
+      }
+    });
+  }
+  const cardMarkup = renderDrinkMarkup(randomDrinks);
+  cocktList.innerHTML = '';
+  cocktList.insertAdjacentHTML('beforeend', cardMarkup);
+}
 
 createLetterList(arr);
 
 createRandomList();
 
 function fetch() {
-    let pages = 0;
-    let btnLoadMore = '';
-    if (letter !== '') {
-        return getCocktailByLetter(letter)
-            .then(data => {
-                if (data.drinks !== null) {   
-                    cocktSectRef.classList.remove('visually-hidden');
-                    notFoundRef.classList.add('not-found');
-                cocktListLength = data.drinks.length;
-                drinksList = data.drinks;
-                const cardMarkup = renderDrinkMarkup(drinksList);
-                cocktList.innerHTML = '';
-                cocktList.insertAdjacentHTML('beforeend', cardMarkup);
-            } else {
-                markupAlert();
-                }
-        })
-    }
+  let pages = 0;
+  let btnLoadMore = '';
+  if (letter !== '') {
+    return getCocktailByLetter(letter).then(data => {
+      if (data.drinks !== null) {
+        cocktSectRef.classList.remove('visually-hidden');
+        notFoundRef.classList.add('not-found');
+        cocktListLength = data.drinks.length;
+        drinksList = data.drinks;
+        const cardMarkup = renderDrinkMarkup(drinksList);
+        cocktList.innerHTML = '';
+        cocktList.insertAdjacentHTML('beforeend', cardMarkup);
+      } else {
+        markupAlert();
+      }
+    });
+  }
 
-    markupAlert();    
-};
+  markupAlert();
+}
 
 function removeActive() {
-    btnLetterRef.forEach(elem => {
-    elem.classList.remove("button-active");
-    })
+  btnLetterRef.forEach(elem => {
+    elem.classList.remove('button-active');
+  });
+}
+
+const handleClick = event => {
+  removeActive();
+  letter = event.target.textContent.trim().toLowerCase();
+  event.target.classList.add('button-active');
+  fetch();
 };
-          
-            
-const handleClick = (event => {
-    removeActive();
-    letter = event.target.textContent.trim().toLowerCase();
-    event.target.classList.add("button-active")
-    fetch();
-});
 
 function markupAlert() {
-    
-    cocktSectRef.classList.add('visually-hidden')
-    notFoundRef.classList.remove('is-hidden');
-
-};
+  cocktSectRef.classList.add('visually-hidden');
+  notFoundRef.classList.remove('is-hidden');
+}
 
 letterListRef.addEventListener('click', handleClick);
 formByLetter.addEventListener('click', handleClick);
 
-
-
 // модалка
 
 function modalSelector() {
-    const refs = {
+  const refs = {
     openModalBtn: document.querySelector('.cocktails__list'),
-    };
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    
-    document.querySelector('.back-drop').addEventListener('click', event => {
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
 
-        if (event.target === event.currentTarget || event.target === document.querySelector('.modal__button') ) {
-            closeAuthModal();
-        } else if (event.target.parentNode.parentNode === document.querySelector('.cocktail-compound')) {
-            document.querySelector('.modal__ingredients').parentNode.classList.remove('is-hidden');
-            ingrModalOpen(event);
-        };
-    });
-    function closeAuthModal() {
-      document.querySelector('.back-drop').classList.add('is-hidden');
-    } 
-};
+  document.querySelector('.back-drop').addEventListener('click', event => {
+    if (
+      event.target === event.currentTarget ||
+      event.target === document.querySelector('.modal__button')
+    ) {
+      closeAuthModal();
+    } else if (
+      event.target.parentNode.parentNode ===
+      document.querySelector('.cocktail-compound')
+    ) {
+      document
+        .querySelector('.modal__ingredients')
+        .parentNode.classList.remove('is-hidden');
+      ingrModalOpen(event);
+    }
+  });
+  function closeAuthModal() {
+    document.querySelector('.back-drop').classList.add('is-hidden');
+  }
+}
 
 modalSelector();
 
-
 function toggleModal(event) {
-    const modal = document.querySelector("[data-modal]");
-    
-    if (event.target.textContent !== 'Learn more') {
-        return
+  const modal = document.querySelector('[data-modal]');
+
+  if (event.target.textContent !== 'Learn more') {
+    return;
+  }
+  cocktId = event.target.parentNode.previousElementSibling.dataset.id;
+
+  getCocktailById(cocktId).then(data => {
+    if (data.drinks !== null) {
+      cocktListLength = data.drinks.length;
+      drinksList = data.drinks;
+
+      let cocktObj = data.drinks[0];
+      let ingrObj = {};
+      let dozsObj = {};
+
+      for (let key in cocktObj) {
+        if (key.includes('strIngredient')) {
+          if (cocktObj[key] !== null) {
+            ingrObj[key] = cocktObj[key];
+          }
+        }
+      }
+
+      for (let key in cocktObj) {
+        if (key.includes('strMeasure')) {
+          if (cocktObj[key]) {
+            dozsObj[key] = cocktObj[key];
+          }
+        }
+      }
+
+      for (let i = 0; i <= Object.keys(ingrObj).length - 1; i += 1) {
+        ingrMarkup += `<li><a class="compound__elem" href="" data-ingredient="${
+          Object.values(ingrObj)[i]
+        }">${Object.values(dozsObj)[i]} ${Object.values(ingrObj)[i]}</a></li>
+                    `;
+      }
+      renderModalCockt(drinksList);
+    } else {
+      markupAlert();
     }
-    cocktId = event.target.parentNode.previousElementSibling.dataset.id;
-    
-    getCocktailById(cocktId)
-        .then(data => {
-            if (data.drinks !== null) {
-                cocktListLength = data.drinks.length;
-                drinksList = data.drinks;
-                
-                let cocktObj = data.drinks[0];
-                let ingrObj = {};
-                let dozsObj = {};
+  });
+  modal.classList.toggle('is-hidden');
+}
 
-                for (let key in cocktObj) {
-                    if (key.includes('strIngredient')) {
-                        if (cocktObj[key] !== null) {
-                            ingrObj[key] = cocktObj[key]
-                        } 
-                    }
-                }
-
-                for (let key in cocktObj) {
-                    if (key.includes('strMeasure')) {
-                        if (cocktObj[key]) {
-                            dozsObj[key] = cocktObj[key]
-                        } 
-                    }
-                }
-    
-                for (let i = 0; i <= Object.keys(ingrObj).length - 1; i += 1) {
-                    ingrMarkup += `<li><a class="compound__elem" href="" data-ingredient="${Object.values(ingrObj)[i]}">${Object.values(dozsObj)[i]} ${Object.values(ingrObj)[i]}</a></li>
-                    `}
-                renderModalCockt(drinksList);
-            } else {
-                markupAlert();
-            }
-        })
-    modal.classList.toggle("is-hidden");
-    
-};
-
+let renderBtn = '';
 function renderModalCockt(drinksList) {
-    let renderBtn = '';
-    const cocktMarkup = drinksList.map(({ idDrink, strInstructions, strDrinkThumb, strDrink }) => {    
-        if (arrCocktls.includes(idDrink) || nameCocktails.includes(idDrink)) {
-            renderBtn = '<button class="btn-re-fav" type="button">Remove from favorite</button>';
-        } else {
-            renderBtn = '<button class="btn-add-fav" type="button">Add to favorite</button>';
-            } 
+  const cocktMarkup = drinksList
+    .map(({ idDrink, strInstructions, strDrinkThumb, strDrink }) => {
+      if (arrCocktls.includes(idDrink) || nameCocktails.includes(idDrink)) {
+        renderBtn =
+          '<button class="btn-re-fav" type="button">Remove from favorite</button>';
+      } else {
+        renderBtn =
+          '<button class="btn-add-fav" type="button">Add to favorite</button>';
+      }
 
-        return `
+      return `
         <div class="modal__path">
           <div class="path__box">
             <h2 class="modal__heading">${strDrink}</h3>
@@ -270,52 +268,57 @@ function renderModalCockt(drinksList) {
             <use href="./image/symbol-defs.svg#icon-close-line"></use>
           </svg>
         </button>
-        `;           
+        `;
     })
-        .join('');
-    
-    cocktModalRef.innerHTML = '';   
-    cocktModalRef.insertAdjacentHTML('beforeend', cocktMarkup);
-    ingrMarkup = '';
-    const ingrRef = document.querySelector('.cocktail-compound');
-    ingrRef.addEventListener('click', ingrModalOpen);    
+    .join('');
+
+  cocktModalRef.innerHTML = '';
+  cocktModalRef.insertAdjacentHTML('beforeend', cocktMarkup);
+  ingrMarkup = '';
+  const ingrRef = document.querySelector('.cocktail-compound');
+  ingrRef.addEventListener('click', ingrModalOpen);
 }
 
 // Вторая модалка
 
 function ingrModalOpen(event) {
-    event.preventDefault();
-    const ingrName = event.target.dataset.ingredient;
-   
-    getIngrByName(ingrName).then(data => {
-        const ingrData = data.ingredients;
-        ingrCardMarkup = ingrData.map(({ idIngredient, strDescription, strType, strABV }) => {
-            
-            let ingrType = 'Non-specific';
-            let ingrDescr = 'No description';
-            let ingrAlcBV = '';
-            let ingrStartDescr = '';
+  event.preventDefault();
+  const ingrName = event.target.dataset.ingredient;
 
-            if (strDescription !== null) {
-                ingrStartDescr = strDescription.split(" ")[0];
-                ingrDescr = strDescription.split(" ").slice(1).join(" ");                
-            };
-            
-            if (strType !== null) {
-                ingrType = strType;   
-            };
-            
-            if (strABV !== null) {
-                ingrAlcBV = `<li class="compound__elem">Alcohol by volume: ${strABV}</li>`;
-            }
-            
-            if (arrIngreds.includes(idIngredient) || arrIdIngridients.includes(idIngredient)) {
-                renderBtn = '<button class="btn-re-fav" type="button">Remove from favorite</button>';
-            } else {
-                renderBtn = '<button class="btn-add-fav" type="button">Add to favorite</button>';
-            }
-        
-            return `
+  getIngrByName(ingrName).then(data => {
+    const ingrData = data.ingredients;
+    ingrCardMarkup = ingrData
+      .map(({ idIngredient, strDescription, strType, strABV }) => {
+        let ingrType = 'Non-specific';
+        let ingrDescr = 'No description';
+        let ingrAlcBV = '';
+        let ingrStartDescr = '';
+
+        if (strDescription !== null) {
+          ingrStartDescr = strDescription.split(' ')[0];
+          ingrDescr = strDescription.split(' ').slice(1).join(' ');
+        }
+
+        if (strType !== null) {
+          ingrType = strType;
+        }
+
+        if (strABV !== null) {
+          ingrAlcBV = `<li class="compound__elem">Alcohol by volume: ${strABV}</li>`;
+        }
+
+        if (
+          arrIngreds.includes(idIngredient) ||
+          arrIdIngridients.includes(idIngredient)
+        ) {
+          renderBtn =
+            '<button class="btn-re-fav" type="button">Remove from favorite</button>';
+        } else {
+          renderBtn =
+            '<button class="btn-add-fav" type="button">Add to favorite</button>';
+        }
+
+        return `
         <div class="modal__title">
             <h2 class="modal__heading">${ingrName}</h2>
             <h3 class="ingredient__title">${ingrType}</h3>
@@ -344,24 +347,30 @@ function ingrModalOpen(event) {
           </svg>
         </button>
             `;
+      })
+      .join('');
 
-        }).join('');
-
-        const ingrModalRef = document.querySelector('.modal__ingredients');
-        ingrModalRef.innerHTML = '';
-        ingrModalRef.insertAdjacentHTML('beforeend', ingrCardMarkup);
-        ingrCardMarkup = '';
-        cocktModalRef.addEventListener('click', ingrModalOpen);
-        document.querySelector('.modal__ingredients').parentNode.addEventListener('click', e => {
-            if (e.target === document.querySelector('.modal__ingredients').parentNode || event.target === document.querySelector('.modal__button')) {
-        closeAuthModal();
+    const ingrModalRef = document.querySelector('.modal__ingredients');
+    ingrModalRef.innerHTML = '';
+    ingrModalRef.insertAdjacentHTML('beforeend', ingrCardMarkup);
+    ingrCardMarkup = '';
+    cocktModalRef.addEventListener('click', ingrModalOpen);
+    document
+      .querySelector('.modal__ingredients')
+      .parentNode.addEventListener('click', e => {
+        if (
+          e.target ===
+            document.querySelector('.modal__ingredients').parentNode ||
+          event.target === document.querySelector('.modal__button')
+        ) {
+          closeAuthModal();
         }
-        });
+      });
 
-        function closeAuthModal() {
-        document.querySelector('.modal__ingredients').parentNode.classList.add('is-hidden');
-        }
-    });
-};
-
-
+    function closeAuthModal() {
+      document
+        .querySelector('.modal__ingredients')
+        .parentNode.classList.add('is-hidden');
+    }
+  });
+}
